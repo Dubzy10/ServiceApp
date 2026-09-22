93a653cabc7f4da07d77337140aaa7e0093a46bc
GRIGS <dubzyyy1000@gmail.com>
2026-09-22 17:45:51 +0300

    made the sql connection and created the datebase for the app and added the logic to make a repair request

--- ServiceApp/Form1.cs
+++ ServiceApp/Form1.cs
@@ -1,14 +1,15 @@
 using ServiceApp.Models;
 using System.Text.RegularExpressions;
+using Microsoft.Data.SqlClient;
 
 namespace ServiceApp;
 
 public partial class Form1 : Form
 {
-    public List<RepairRequest> fakeDataBase = new List<RepairRequest>();
     public Form1()
     {
         InitializeComponent();
+        DataBaseManager.TestConnection();
     }
     
     private void lblDeviceIssue_Click(object sender, EventArgs e)
@@ -21,6 +22,12 @@ public partial class Form1 : Form
     
     private void btnCreateRepairRequest_Click_1(object sender, EventArgs e)
     {
+        string clientName = txtBoxClientName.Text;
+        string phone = txtBoxClientPhoneNumber.Text;
+        string email = txtBoxClientEmailAddress.Text;
+        string device = txtBoxDeviceName.Text;
+        string issue = txtBoxIssueDescription.Text;
+        
         if (string.IsNullOrEmpty(txtBoxClientName.Text) ||
             string.IsNullOrEmpty(txtBoxDeviceName.Text) ||
             string.IsNullOrEmpty(txtBoxClientEmailAddress.Text) ||
@@ -32,10 +39,6 @@ public partial class Form1 : Form
 
             return;
         }
-        
-        string clientName = txtBoxClientName.Text;
-        string phone = txtBoxClientPhoneNumber.Text;
-        string email = txtBoxClientEmailAddress.Text;
 
         if (!Regex.IsMatch(phone, @"^08\d{8}$"))
         {
@@ -51,16 +54,52 @@ public partial class Form1 : Form
             
             return;
         }
-        
-        string device = txtBoxDeviceName.Text;
-        string issue = txtBoxIssueDescription.Text;
 
-        Client currentClient = new Client(clientName, phone, email);
-        RepairRequest currentRequest = new RepairRequest(currentClient, device, issue);
+        try
+        {
+            using (SqlConnection conn = DataBaseManager.GetConnection())
+            {
+                conn.Open();
 
-        fakeDataBase.Add(currentRequest);
+                string insertClientQuery = @"
+                    INSERT INTO  Clients (Full_Name, Phone_Number, Email)
+                    OUTPUT INSERTED.ID
+                    VALUES (@Full_Name, @Phone_Number, @Email);";
+                
+                string insertRequestQuery = @"
+                    INSERT INTO REPAIR_REQUEST (Client_ID, Device_Name, Issue_Description)
+                    VALUES (@Client_ID, @Device_Name, @Issue_Description);";
 
-        MessageBox.Show($"Successful repair request!\n\nClient: {clientName} has a: {device} issue.");
+                int clientID = 0;
+
+                using (SqlCommand cmd = new SqlCommand(insertClientQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Full_Name", clientName);
+                    cmd.Parameters.AddWithValue("@Phone_Number", phone);
+                    cmd.Parameters.AddWithValue("@Email", email);
+
+                    clientID = (int)cmd.ExecuteScalar();
+                }
+                
+                using (SqlCommand cmdReq = new SqlCommand(insertRequestQuery, conn))
+                {
+                    cmdReq.Parameters.AddWithValue("@Client_ID", clientID);
+                    cmdReq.Parameters.AddWithValue("@Device_Name", device);
+                    cmdReq.Parameters.AddWithValue("Issue_Description", issue);
+
+                    cmdReq.ExecuteNonQuery();
+                } 
+
+                MessageBox.Show($"Successful client entry with ID: {clientID}!", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error during client entry with {ex.Message}", "Error", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
     }
 
     private void txtBoxIssueDescription_TextChanged(object sender, EventArgs e)
---
+++ ServiceApp/Models/DataBaseManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ServiceApp.Models;
+
+public class DataBaseManager
+{
+    private static readonly string connectionString = @"Server=.;Database=ServiceAppDB;Integrated Security=True;
+    Encrypt = False;";
+
+    public static SqlConnection GetConnection()
+    {
+        return new SqlConnection(connectionString);
+    }
+
+    public static void TestConnection()
+    {
+        try
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MessageBox.Show("Successful connection with Database!", "Success", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        catch (Exception e)
+        {
+            MessageBox.Show($"Error: {e.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
